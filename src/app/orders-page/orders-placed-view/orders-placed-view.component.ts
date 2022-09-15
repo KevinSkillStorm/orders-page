b8b28dc06af2aa238ad97cbc0aa0266bde8a936a
trwortley <wortley@hey.com>
2022-09-15 15:37:59 -0500

    swapped table with primeng table

--- src/app/orders-page/orders-placed-view/orders-placed-view.component.ts
+++ src/app/orders-page/orders-placed-view/orders-placed-view.component.ts
@@ -8,7 +8,31 @@ import { OrdersPageService } from '../orders-page.service';
   styleUrls: ['./orders-placed-view.component.css']
 })
 export class OrdersPlacedViewComponent implements OnInit {
-  orders: Orders[] = [];
+
+  orders = [
+    {
+      id: 1,
+      email: "Body Wash",
+      price: 2.99,
+      quantity: 2,
+      address: "123 Address St"
+    },
+    {
+      id: 2,
+      email: "Shampoo",
+      price: 1.99,
+      quantity: 1,
+      address: "456 Fake Rd"
+    },
+    {
+      id: 3,
+      email: "Conditioner",
+      price: 3.99,
+      quantity: 5,
+      address: "789 Dummy Ct"
+    }
+  ];
+
   constructor(
     private ordersPageService: OrdersPageService
   ) { }
@@ -17,6 +41,11 @@ export class OrdersPlacedViewComponent implements OnInit {
     // this.getSellerOrder(1);
   }
   getSellerOrder(id: number){
-    this.ordersPageService.getSellerOrders(id).subscribe(orders => this.orders = orders);
+    // this.ordersPageService.getSellerOrders(id).subscribe(orders => this.orders = orders);
   }
+
+  fulfill() {
+    
+  }
+
 }
